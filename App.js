c0ae6b2d2ac3a30706d6e8570fae847820a952a3
chechell <143719679+chechell@users.noreply.github.com>
2023-10-19 11:00:20 -0300

    brooklyn nets
    fe

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text> ef3o47y38tr9w4h5t89gw4y5t9hw495h8t49u5t ;)</Text>
+      <Text>BROOKLYN NETS ;</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
